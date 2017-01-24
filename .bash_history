47fdc4388e9811aeffb4480de28d24f95ac7768e
Quinn Anderson <dingo.ut@gmail.com>
2017-01-23 18:44:22 -0700

    Create a DbContext to connect to Database

---
+++ .bash_history
@@ -0,0 +1,7 @@
+ls
+cd .git
+ls
+ssh-keygen -t rsa -b 4096 -C dingo.ut@gmail.com
+ls
+cd /
+ls
---
+++ ToDoList/ToDoList/Models/ToDoApp/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models.ToDoApp
+{
+    public class Item
+    {
+        public int itemID { get; set; }
+        public string itemName { get; set; }
+        public int listID { get; set; }
+    }
+}
